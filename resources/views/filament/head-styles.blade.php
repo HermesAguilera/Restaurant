3c6e9b56e79869fcc216cc34b09977c54bf6ee01
HermesAguilera <hermes99aguilera@gmail.com>
2026-06-15 15:38:10 -0600

    ptm mas

--- resources/views/filament/head-styles.blade.php
+++ resources/views/filament/head-styles.blade.php
@@ -1,2 +1,3 @@
 <link rel="stylesheet" href="{{ asset('css/filament/filament/app.css') }}">
+<link rel="stylesheet" href="{{ asset('css/filament/filament/custom.css') }}">
 <link rel="stylesheet" href="{{ asset('css/filament/admin/theme.css') }}">
